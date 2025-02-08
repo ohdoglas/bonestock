42b9b8b39f75c84facf6ace4a434f08912696f84
ohdoglas <dughlaisdd@gmail.com>
2025-02-08 11:29:29 -0300

    config/feat: express initial config | server running well

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,14 +1,18 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    ignores: ["node_modules/", "dist/", "build/", "data/postgres/"],  // Agora usa "ignores"
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"], // Sem JSX ou TSX
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ];
