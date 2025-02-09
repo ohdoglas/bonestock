f9951cfccd54585ff33274b447f05b82b95e91af
ohdoglas <dughlaisdd@gmail.com>
2025-02-09 19:42:37 -0300

    feat: setup messages added

---
+++ backend/src/utils/messages/setupMsg.ts
@@ -0,0 +1,15 @@
+const SETUP = {
+    ERR: {
+        MISSING_FIELDS: "",
+        INVALID_TOKEN: "O token fornecido não é válido.",
+        SEND_EMAIL_FAILED: "Falha ao enviar e-mail de confirmação.",
+        MISSING_MAIL_ENV: "ADMIN_MAIL não configurado."
+    },
+    SUCCESS: {
+        ALREADY_COMPLETED: "A configuração inicial para o super-admin já foi completada.",
+        ADMIN_CREATED: "Super-admin criado com sucesso! Para acessar o sistema, por favor, verifique seu e-mail e clique no link enviado. Caso não encontre o e-mail, verifique a caixa de spam ou a lixeira. Se o problema não for resolvido, por favor, contate o nosso suporte."
+
+    }
+}
+
+export default SETUP;
