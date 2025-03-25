160f314eb1c65a69b90b1eb45ef7be0e2ae04408
ohdoglas <dughlaisdd@gmail.com>
2025-03-24 21:13:44 -0300

    feat: jwt global middleware authenticator implemented

---
+++ backend/src/middlewares/authenticateToken.ts
@@ -0,0 +1,40 @@
+import jwt from 'jsonwebtoken';
+import { NextFunction, Response } from "express";
+import TRequestUserID from "../types/TRequest";
+import SERVER from "../utils/messages/serverMessages";
+import User from '../models/user';
+
+
+const JWT_SECRET = process.env.JWT_SECRET || "";
+
+export async function authenticateToken(req: TRequestUserID, res: Response, next: NextFunction) {
+    const authReq = req.headers.authorization;
+    const token = authReq && authReq.split(' ')[1];
+
+    try {
+        if (!token) {
+            return res.status(401).json({
+                message: SERVER.ERR.MISSING_TOKEN
+            });
+        }
+
+        const decoded = jwt.verify(token, JWT_SECRET) as { sub: string };
+        const userExists = await User.findById(decoded.sub);
+
+        if (!userExists) {
+            return res.status(404).json({
+                message: SERVER.ERR.USER_NOT_FOUND
+            });
+        }
+
+        req.userId = decoded.sub;
+
+        next();
+    } catch (error) {
+        const erro = error as Error;
+        console.error(erro);
+        return res.status(500).json({
+            message: SERVER.ERR.INTERNAL_ERROR
+        });
+    }
+}
--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 import initSetupRoute from "./serverSide/initSetupRoute";
 import { userRoute } from "./userRoutes";
+import { authenticateToken } from "../middlewares/authenticateToken";
 
 const routes = Router();
 
@@ -12,5 +13,12 @@ routes.get('/', (req: Request, res: Response) => {
 
 routes.use('/', initSetupRoute)
 routes.use('/', userRoute);
+routes.use(authenticateToken);
+
+routes.get('/tokentest', (req: Request, res: Response) => {
+    return res.status(200).json({
+        message: "Token Test OK"
+    });
+});
 
 export default routes;
